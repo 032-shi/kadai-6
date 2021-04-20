ddd5ddcc89a02d302a691e4421a7dd89582661d5
032-shi <0324omitsu@gmail.com>
2021-04-20 03:26:40 +0000

    [update]4-10

--- db/migrate/20210418104806_devise_create_users.rb
+++ db/migrate/20210418104806_devise_create_users.rb
@@ -32,7 +32,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :name #nemeカラムを
+      t.string :name #nameカラムを追加
+      t.string :introduction #introductionカラムを追加
+
 
       t.timestamps null: false
     end
