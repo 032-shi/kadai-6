87853813dff19b66d7ba38d8f9a5b3be4d1230f4
032-shi <0324omitsu@gmail.com>
2021-04-21 09:23:31 +0000

    [update]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'homes/about'
   root to: 'homes#top'
   devise_for :users
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:show, :edit, :update, :index]
 end
