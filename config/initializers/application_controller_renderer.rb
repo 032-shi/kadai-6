bc42193529180217243f4bab6ee2162714140fc6
032-shi <0324omitsu@gmail.com>
2021-04-21 03:04:02 +0000

    [update]4-10

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a45966c7c1850903823a4b8debdbb83ff8be548961bcd4c0716ed8d6e75a9828406726033307897f4a257b949e78f1985224b3832598d7b2ce8fbc6997ce88e0'
