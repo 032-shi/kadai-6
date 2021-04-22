c47a697ff906a59a19cc0a995457a28d20967323
032-shi <0324omitsu@gmail.com>
2021-04-22 02:46:17 +0000

    [update]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,11 +8,15 @@ class BooksController < ApplicationController
   @book.user_id = current_user.id
     if  @book.save
         redirect_to book_path(@book.id), notice: "You have created book successfully."
+    else
+        @books = Book.all
+        render :index
     end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -27,6 +31,8 @@ class BooksController < ApplicationController
   @book = Book.find(params[:id])
     if  @book.update(book_params)
         redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+        render :edit
     end
   end
 
