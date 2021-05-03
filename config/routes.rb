2e86d2d97b357ef161788ce4b7577fe6ff9441e1
032-shi <0324omitsu@gmail.com>
2021-05-03 04:59:39 +0000

    [update]kadai-3

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'home/about'=>'homes#about'
   root to: 'homes#top'
   devise_for :users
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+    resources :book_comments, only: [:create, :destroy]
+  end
+
   resources :users, only: [:show, :edit, :update, :index]
 end
